428749138913812beacbb9f7d26bb7e12d18eda2
hohyun0406 <hohyun0406@gmail.com>
2024-05-28 11:44:09 +0900

    first commit

---
+++ law-back/src/main/java/site/lawmate/lawcase/LawcaseApplication.java
@@ -0,0 +1,13 @@
+package site.lawmate.lawcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawcaseApplication.class, args);
+	}
+
+}
---
+++ law-back/src/test/java/site/lawmate/lawcase/LawcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package site.lawmate.lawcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
